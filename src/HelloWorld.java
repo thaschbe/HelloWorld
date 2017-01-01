bf62a1b4c96616301962141a08ac29c4c7d0c381
thaschbe <thomas.haschberger@aon.at>
2017-01-01 15:16:23 +0100

    main loop

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,16 @@
 
 public class HelloWorld {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World 2 !");
-	}
-
+		int Cnt=0;
+		System.out.println("Starting ...");
+		while (true)
+			{
+			System.out.println("Loop " + Cnt);
+			Cnt++;
+			Thread.sleep(3000);
+			}
+		
+	}	
 }
